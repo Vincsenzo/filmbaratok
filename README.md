4290dddafefa64e7410300539a95854f027c04af
Vince Szabó <szabo.vince@icloud.com>
2024-12-19 22:51:45 +0100

    making wsgi.py run with the production settings

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TODOs;
+# TODOs:
 - pagination on episodes and blogs page
 - diplaying search results better diplayed
 - making album cover better displayed
--- filmbaratok/settings/production.py
+++ filmbaratok/settings/production.py
@@ -2,6 +2,14 @@ from .base import *
 
 DEBUG = False
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-7z%%05px%t!!qe3mrjb=rnn*tu^9ul^1z)&#iqb$y2h17%)yyl"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["filmbaratok.vincsenzo.hu"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
 try:
     from .local import *
 except ImportError:
